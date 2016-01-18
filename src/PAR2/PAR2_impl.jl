14e05590c6bb19948da6c972381b85960619ef47
tan <tanmaykm@gmail.com>
2016-01-18 09:17:28 +0530

    parquet-format thrift types

---
+++ src/PAR2/PAR2_impl.jl
@@ -0,0 +1,5 @@
+# give nicer names to entities that had name clashes
+const PARType = _Type 
+export PARType
+
+# server impleneation intentionally empty as this is just the client part
--- src/Parquet.jl
+++ src/Parquet.jl
@@ -1,5 +1,11 @@
 module Parquet
 
+using Thrift
+
+
 # package code goes here
+
+include("PAR2/PAR2.jl")
+using .PAR2
 
 end # module
