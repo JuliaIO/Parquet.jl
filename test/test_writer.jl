39323df31e4abcc713a6b150733b9a842bfaf30f
xiaodaigh <zhuojia.dai@gmail.com>
2020-05-13 14:09:37 +1000

    adding tests

--- test/test_writer.jl
+++ test/test_writer.jl
@@ -4,7 +4,7 @@ using Random:randstring
 
 tbl = (
     int32 = Int32.(1:1000),
-    int64 = Int32.(1:1000),
+    int64 = Int64.(1:1000),
     float32 = Float32.(1:1000),
     float64 = Float64.(1:1000),
     bool = rand(Bool, 1000),
@@ -14,10 +14,17 @@ tbl = (
     float32m = rand([missing, Float32.(1:100)...], 1000),
     float64m = rand([missing, Float64.(1:100)...], 1000),
     boolm = rand([missing, true, false], 1000),
+    stringm = rand([missing, "abc", "def", "ghi"], 1000)
 )
 
 write_parquet("tmp.parquet", tbl)
 
-ParFile("tmp.parquet")
+pf = ParFile("tmp.parquet")
+col_chunks = columns(pf, 1)
+vals = values.(Ref(pf), Ref(col_chunks), 1:length(col_chunks))
+
+vals = values(pf, col_chunks, 5)
+vals = values(pf, col_chunks, 6)
+vals = values(pf, col_chunks, 7)
 
 rm("tmp.parquet")
