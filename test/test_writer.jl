9021c90561c0d24f80c5f7c3b99f4b4236cc8627
tan <tanmaykm@gmail.com>
2020-05-27 08:38:36 +0530

    fix BatchedColumnsIterator missing value handling
    Looking into definition levels while referencing the values now.

--- test/test_writer.jl
+++ test/test_writer.jl
@@ -9,26 +9,28 @@ end
 Random.seed!(1234567)
 
 function test_write()
+    N = 1000
+
     tbl = (
-        int32 = rand(Int32, 1000),
-        int64 = rand(Int64, 1000),
-        float32 = rand(Float32, 1000),
-        float64 = rand(Float64, 1000),
-        bool = rand(Bool, 1000),
-        string = [randstring(8) for i in 1:1000],
-        int32m = rand([missing, rand(Int32, 10)...], 1000),
-        int64m = rand([missing, rand(Int64, 10)...], 1000),
-        float32m = rand([missing, rand(Float32, 10)...], 1000),
-        float64m = rand([missing, rand(Float64, 10)...], 1000),
-        boolm = rand([missing, true, false], 1000),
-        stringm = rand([missing, "abc", "def", "ghi"], 1000)
+        int32 = rand(Int32, N),
+        int64 = rand(Int64, N),
+        float32 = rand(Float32, N),
+        float64 = rand(Float64, N),
+        bool = rand(Bool, N),
+        string = [randstring(8) for i in 1:N],
+        int32m = rand([missing, rand(Int32, 10)...], N),
+        int64m = rand([missing, rand(Int64, 10)...], N),
+        float32m = rand([missing, rand(Float32, 10)...], N),
+        float64m = rand([missing, rand(Float64, 10)...], N),
+        boolm = rand([missing, true, false], N),
+        stringm = rand([missing, "abc", "def", "ghi"], N)
     )
 
     tmpfile = tempname()*".parquet"
 
     write_parquet(tmpfile, tbl)
 
-    pf = ParFile(tmpfile)
+    pf = ParFile(tmpfile; map_logical_types=true)
 
     # the file is very small so only one rowgroup
     col_chunks = columns(pf, 1)
@@ -49,6 +51,18 @@ function test_write()
         else
             non_missing_vals_read = vals_from_file[1][1:sum(vals_from_file[2])]
             @test all(non_missing_vals .== non_missing_vals_read)
+        end
+    end
+
+    # test with BatchedColumnsCursor
+    cc = Parquet.BatchedColumnsCursor(pf)
+    vals, _ = iterate(cc)
+    @test length(vals) == 12
+    @test length(vals.boolm) == N
+    for idx in 1:N
+        @test ismissing(vals.boolm[idx]) == ismissing(tbl.boolm[idx])
+        if !ismissing(vals.boolm[idx])
+            @test vals.boolm[idx] == tbl.boolm[idx]
         end
     end
 
