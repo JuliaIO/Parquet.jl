55a9293c7f603044ad47ebaac5f903160244c98e
Mosè Giordano <mose@gnu.org>
2022-06-25 14:13:23 +0100

    Use artifacts for fetching testing data
    This avoids git cloning repositories every single time in CI.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,11 @@
-include("get_parcompat.jl")
+using Parquet
+using Test
+using LazyArtifacts, Artifacts
+
+# Note: readdir(...; join=true) requires Julia v1.4.
+const parcompat = joinpath(artifact"parcompat", only(readdir(artifact"parcompat")))
+const julia_parcompat = joinpath(artifact"julia_parcompat", only(readdir(artifact"julia_parcompat")))
+
 @testset "parquet tests" begin
     include("test_load.jl")
     include("test_codec.jl")
